bfb7d171d0b5933479f3b9d356fca9eb292b9635
Leah Baker <leah@leahs-mbp.lan>
2020-05-26 11:06:26 -0700

    added circle component and got it rendering through app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
+import Circle from './Circle/Circle';
 import './App.css';
+
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Circle />
     </div>
   );
 }
---
+++ src/Circle/Circle.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Circle extends Component {  
+    render() {
+        return(
+            <p>Testing seperate circle compeonent</p>
+        )
+    }
+}
