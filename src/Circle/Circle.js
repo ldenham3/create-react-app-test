f236658b97be7bcb27bc4d1dbf780d380225a2e4
Leah Baker <leah@leahs-mbp.lan>
2020-05-26 13:44:19 -0700

    circle component now rendering circle on the app page as well. still need to position it over the  images, rotate the carousel on click, add labels, move it on each image, etc

--- src/Circle/Circle.js
+++ src/Circle/Circle.js
@@ -3,8 +3,14 @@ import './Circle.css';
 
 export default class Circle extends Component {  
     render() {
-        return(
-            <p>Testing seperate circle component</p>
-        )
+        var circleStyle = {
+            borderRadius: "50%",
+            width:100,
+            height:100,
+            border:"5px solid red",
+        };
+        return (
+            <div style={circleStyle}></div>
+        );
     }
 }
